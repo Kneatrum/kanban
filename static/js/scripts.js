c8b186e15eaffbba2142741986bb514e1ef35a71
Kneatrum <martin.mwiti.n@gmail.com>
2023-12-12 21:27:15 +0300

    Improved the method of implementing drag and drop

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,68 +1,41 @@
-var dragged; // Variable to store the dragged element
 
-// Function to allow dropping
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const columns = document.querySelectorAll('.column');
+    let draggedItem = null;
 
-// Function to handle drag start
-function drag(ev) {
-    dragged = ev.target; // Store the dragged element
-}
+    columns.forEach(column => {
+        column.addEventListener('dragstart', event => {
+            draggedItem = event.target.closest('.card');
+            if (!draggedItem) return;
+            event.dataTransfer.setData('text/plain', null); // Required for Firefox
+        });
 
-// Function to handle drop
-function drop(ev) {
-    ev.preventDefault();
+        column.addEventListener('dragover', event => {
+            event.preventDefault();
+        });
 
-    // Check if the drop target is outside the columns or above an 'h2' element
-    if (
-        !ev.target.classList.contains('column') ||  // Prevent dropping outside columns
-        ev.target.tagName.toLowerCase() === 'h2'   // Prevent dropping above 'h2' elements
-    ) {
-        return; // Exit the function, preventing the drop
-    }
+        column.addEventListener('drop', event => {
+            const dropTarget = event.target.closest('.column');
+            if (!dropTarget) return;
 
-    var dropTarget = ev.target;
-    var dropPosition = -1; // Initialize drop position as before the first child
+            if (draggedItem) {
+                const dropPosition = event.clientY;
+                const tasks = Array.from(dropTarget.querySelectorAll('.card'));
+                const closestTask = tasks.reduce((closest, task) => {
+                    const rect = task.getBoundingClientRect();
+                    const offset = dropPosition - rect.top - rect.height / 2;
+                    if (offset < 0 && (!closest || offset > closest.offset)) {
+                        return { task, offset };
+                    }
+                    return closest;
+                }, null);
 
-    // Determine the drop position
-    var rect = dropTarget.getBoundingClientRect();
-    if (ev.clientY > rect.top + rect.height / 2) {
-        dropPosition = 1; // Insert after the drop target if below the mid-point
-    }
-
-    // Insert the dragged element at the correct position within the column
-    if (dropPosition === 1) {
-        dropTarget.appendChild(dragged);
-    } else {
-        var tasks = dropTarget.getElementsByClassName('card');
-        var taskDropped = false;
-
-        for (var i = 0; i < tasks.length; i++) {
-            var taskRect = tasks[i].getBoundingClientRect();
-            if (ev.clientY < taskRect.top) {
-                dropTarget.insertBefore(dragged, tasks[i]);
-                taskDropped = true;
-                break;
+                if (closestTask) {
+                    dropTarget.insertBefore(draggedItem, closestTask.task);
+                } else {
+                    dropTarget.appendChild(draggedItem);
+                }
             }
-        }
-
-        if (!taskDropped) {
-            dropTarget.appendChild(dragged);
-        }
-    }
-}
-
-
-
-
-// Event listener for drag start
-document.addEventListener('dragstart', drag);
-
-// Event listener for drag over
-document.addEventListener('dragover', allowDrop);
-
-// Event listener for drop
-document.addEventListener('drop', drop);
-
-
+        });
+    });
+});
